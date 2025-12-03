9cef45e1859a78a9cba04862d1dcdb199e676f15
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-12-04 00:36:17 +0530

    check

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -21,6 +21,7 @@ export default function Main() {
     }
 
     async function getRecipe() {
+        console.log("clicked");
         setLoading(true);
         setRecipe(null);
 
@@ -39,7 +40,7 @@ export default function Main() {
         } finally {
             setLoading(false);
         }
-        
+
     }
 
     return (
