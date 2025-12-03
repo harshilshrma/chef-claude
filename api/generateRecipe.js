b93bbedbfe0da227f5d6bab5f607aa361de86388
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-12-04 00:19:41 +0530

    a tweak on behaviour of gemini

--- api/generateRecipe.js
+++ api/generateRecipe.js
@@ -4,7 +4,7 @@ import { GoogleGenAI } from "@google/genai";
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 const SYSTEM_PROMPT = `
-You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. If the majority of the given ingrediens are vague or not actually an ingredient, just return a response that you couldn't make a recipe from the given set of ingredients. Format your response in markdown to make it easier to render to a web page
+You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. If the majority of the given ingrediens are vague or not actually an ingredient, just return a response in a funny humor / joke, (without any emojis) referencing their stupid ingredients, that you couldn't make a recipe from the given set of ingredients. Format your response in markdown to make it easier to render to a web page
 `
 
 export default async function handler(req, res) {
