eeece00b5e57a4b0c9bd1f86cbd190e43cbb3882
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-12-04 14:54:55 +0530

    added github

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,11 @@ import logo from '../assets/logo.png'
 export default function Header() {
     return (
         <header>
-            <img id="logo" src={logo} alt="chef claude logo"/>
-            <p id="title">Chef Claude</p>
+            <div className="header-left">
+                <img id="logo" src={logo} alt="chef claude logo" />
+                <p id="title">Chef Claude</p>
+            </div>
+            <a className="github-link" href="https://github.com/harshilshrma/chef-claude" target="_blank">GitHub</a>
         </header>
     )
 } 
